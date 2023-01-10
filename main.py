2b36ae69e668f6490398fe166f3ed07950b09f8e
Flowka <igorkovt2007@gmail.com>
2023-01-10 18:02:31 +0200

    root init

---
+++ main.py
@@ -0,0 +1,15 @@
+from threading import Thread
+import customtkinter as ctk
+import tkinter as tk
+import os
+
+
+def main():
+    root = ctk.CTk()
+    root.geometry('900x500')
+    root.resizable(False, False)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()

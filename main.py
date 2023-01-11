1ef2a65a608f5db6dc2cb1766fe8ea8acf958a9c
Flowka <igorkovt2007@gmail.com>
2023-01-11 15:20:29 +0200

    added entry_link wout func

--- main.py
+++ main.py
@@ -4,12 +4,28 @@ import tkinter as tk
 import os
 
 
+class StartWindow():
+    def __init__(self, root):
+        self.root = root
+
+        self.frame_main = ctk.CTkFrame(root, fg_color = None)
+        self.frame_main.pack(expand = True)
+
+        self.entry_link = ctk.CTkEntry(self.frame_main, justify = 'center', width = 500)
+        self.entry_link.pack(side = 'top')
+
+        self.label_WhToDo = ctk.CTkLabel(self.frame_main, text = 'Put your utube link here ↑')
+        self.label_WhToDo.pack(side = 'top')
+
+
 def main():
     root = ctk.CTk()
     root.geometry('900x500')
     root.resizable(False, False)
 
     root.attributes('-alpha', 0.96)
+
+    StartWindow(root)
 
     root.mainloop()
 
